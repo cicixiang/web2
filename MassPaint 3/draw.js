05ef814ae3e364d84544e362a7aa95eccea0c40e
cicixiang <xchengcheng@massart.edu>
2015-03-23 11:32:42 -0400

    2

---
+++ MassPaint 3/draw.js
@@ -0,0 +1,125 @@
+speed(0);
+record();
+reset();
+hide();
+
+
+
+$("#button-save").on("click",function() {
+ downloadRecording();
+});
+
+$("#button-replay").on("click",function() {
+ speed(0)
+ replay();
+ speed(0)
+});
+
+$("#button-eraser").on("click",function() {
+  $(".tool.selected").removeClass("selected");
+  $(this).addClass("selected");
+  tool = "eraser";
+ lineWidth(30);
+ lineColor("white");
+});
+
+$("#button-pen").on("click",function() {
+  $(".tool.selected").removeClass("selected");
+  $(this).addClass("selected");
+  tool = "pen";
+ lineWidth(width);
+ lineColor(color);
+});
+
+$("#button-brush").on("click",function() {
+  $(".tool.selected").removeClass("selected");
+  $(this).addClass("selected");
+  tool = "brush";
+ lineWidth(60);
+ lineColor("black");
+});
+
+
+$("#button-sun").on("click",function() {
+ $(".tool.selected").removeClass("selected");
+ $(this).addClass("selected");
+ tool = "sun";
+ lineWidth(90);
+ lineColor("yellow");
+});
+
+$("#button-sea").on("click",function() {
+ $(".tool.selected").removeClass("selected");
+ $(this).addClass("selected");
+ lineColor("#60ffff");
+ lineWidth(5);
+ tool = "sea";
+});
+
+$("#button-beach").on("click",function() {
+ $(".tool.selected").removeClass("selected");
+ $(this).addClass("selected");
+ lineColor("#ffc020");
+ lineWidth(100);
+ tool = "beach";
+});
+
+$("#button-tree").on("click",function() {
+ $(".tool.selected").removeClass("selected");
+ $(this).addClass("selected");
+ lineColor("#408020");
+ lineWidth(5);
+ tool = "tree";
+});
+
+$("#button-trunk").on("click",function() {
+ $(".tool.selected").removeClass("selected");
+ $(this).addClass("selected");
+ lineColor("#806000");
+ lineWidth(15);
+ tool = "trunk";
+});
+
+
+function sun() {
+	//arc(e.pageX, e.pageY, 10, 0, 360, true);
+	angle(0);
+	for (i=0; i<360; i++){
+		forward(0.5);
+		rotate(1);
+	}
+}
+
+
+var drawing = false;
+var tool = "pen";
+
+var color = "black";
+var width = 4;
+
+$(document).on("mousedown",function(e) {
+  e.preventDefault();
+  moveTo(e.pageX,e.pageY);
+  if(tool == "pen" || tool =="eraser" || tool =="brush" || tool == "sea" || tool == "beach" || tool == "tree" || tool == "trunk" ) { 
+	  drawing = true;
+  } else if(tool == "sun") {
+    sun();
+  }
+});
+
+
+$(document).on("mousemove",function(e) {
+  e.preventDefault();
+  if(drawing) {
+    lineTo(e.pageX,e.pageY);
+  }
+});
+
+$(document).on("mouseup",function(e) {
+  e.preventDefault();
+  drawing = false;
+});
+
+
+
+
