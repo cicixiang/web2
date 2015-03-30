29eb2bcba708073df7e9fa995c15114c4e455c85
cicixiang <xchengcheng@massart.edu>
2015-03-30 15:49:02 -0400

    hm
    hm

--- hm/js.js
+++ hm/js.js
@@ -3,15 +3,39 @@
 function addImage(image, link, like, caption){
 
 	var $div = $("<div>");
-	var $Annotation = $("<h1>").text(caption);
-	var $LINK = $("<a>").attr("href", link);
-	var $IMAGE = $("<img>").attr("src", image);
-	var $LIKE = $("<span>").text(like);
+	var $caption = $("<h1>").text(caption);
+	var $link = $("<a>").attr("href", link);
+	var $image = $("<img>").attr("src", image);
+	var $like = $("<span>").text(like);
+
 	
-	var $image_link = $LINK.append($IMAGE);
-	
-	$div.append($LINK).append($image_link).append($LIKE);
-	
+	$link.append($image);
+	$div.append($link).append($image).append($like).append($caption);
+	$("#container").append($div);
 }
 
-addImage("http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055772_1558157594448054_879696703_n.jpg", "https://instagram.com/p/0lOFZ0g8gA/", 720, "just for test");
+addImage("http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055772_1558157594448054_879696703_n.jpg",
+        "https://instagram.com/p/0lOFZ0g8gA/",
+   "720", 
+		"منشن الي تبيه يشوف المقطع"
+
+	);
+
+addImage("http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055442_1387234548264057_209258360_n.jpg",
+        "https://instagram.com/p/0lS-LWTbw9/",
+        "432", "RCF x VFS2 - pic via @kdot_vossen #FreeBuju #Vossen #CarsWithoutLimits");
+addImage("http://scontent.cdninstagram.com/hphotos-xfa1/t51.2885-15/s150x150/e15/10919675_944043618953681_1961747465_n.jpg",
+        "https://instagram.com/p/0lTX-pqkcC/",
+        "543", "Сегодня была на прекрасном мероприятии #soundkids");
+addImage("http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055616_1568588500090432_546823562_n.jpg",
+        "https://instagram.com/p/0lNgG4st5V/",
+        "344", "Are your tubes in good shape?");
+
+
+function drawImages(data){
+	var item = data.data[6];
+	console.log(item);
+	
+	var image = item.scsdcsdcs;
+	console.log(image);
+}
